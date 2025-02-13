3760d94dfc60cb353cde62160662aa1e74431494
Andile <andilenzmbatha0@gmail.com>
2025-02-13 22:31:10 +0200

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This project was coded by {""}
+          <a href="https://www.linkedin.com/in/andile-mbatha-a56a2915b/">
+            Andile Mbatha
+          </a>
+          {""} and is {""}
+          <a
+            href="https://github.com/ANDILE805/react-weather-app"
+            target="_blank"
+          >
+            Open sourced on Github
+          </a>
+        </footer>
+      </div>
+    </div>
+  );
 }
 
 export default App;
