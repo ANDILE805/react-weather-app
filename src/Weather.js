77e4a9d094148a3f5a377dc8d679142c8fbae02f
Andile <andilenzmbatha0@gmail.com>
2025-02-18 22:58:42 +0200

    Added basic HTML and CSS

---
+++ src/Weather.js
@@ -0,0 +1,48 @@
+import React from "react";
+import "./Weather.css";
+export default function Weather() {
+  return (
+    <div className="Weather">
+      <form>
+        <div className="row">
+          <div className="col-9">
+            <input
+              type="search"
+              placeholder="Enter a city"
+              className="form-control"
+              autoFocus="on"
+            />
+          </div>
+          <div className="col-3">
+            <input
+              type="submit"
+              value="Search"
+              className="btn btn-primary w-100"
+            />
+          </div>
+        </div>
+      </form>
+      <h1>Osizweni</h1>
+      <ul>
+        <li>Wednesday 07:00</li>
+        <li>Mostly Cloudy</li>
+      </ul>
+      <div className="row">
+        <div className="col-6">
+          <img
+            src="https://ssl.gstatic.com/onebox/weather/64/rain.png"
+            alt="Rainy"
+          />
+          18C
+        </div>
+        <div className="col-6">
+          <ul>
+            <li>Precipitation: 10%</li>
+            <li>Humidity: 12%</li>
+            <li>Wind: 10 km/h</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+}
